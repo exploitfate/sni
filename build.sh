33542d7ea88432129b8f4e97e8693cad1a28e257
Casper <casper@inernetbs.net>
2020-09-18 17:14:50 +0300

    Fix build

--- build.sh
+++ build.sh
@@ -261,7 +261,7 @@ if [[ ${SERVICE} == "iptables" ]]; then
 fi
 
 log_action_begin_msg "installing ipset-persistent service"
-sudo apt-get -y install ipset-persistent &>> ${CWD}/sni.log
+sudo apt-get -y -qq install ipset-persistent &>> ${CWD}/sni.log
 log_action_end_msg $?
 
 log_action_begin_msg "reload ${SERVICE}-persistent service to enable ipset-persistent"
